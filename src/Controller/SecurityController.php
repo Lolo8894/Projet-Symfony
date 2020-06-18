30e7bb3ac3722c4d89fc2b12ca1d0bdfcf7ee642
Lolo8894 <lauramassaro.ks@gmail.com>
2020-06-18 19:29:27 +0200

    test commit suite changement de mot de passe (hacking de mon compte... -_-')

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,22 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-
 use App\Entity\Utilisateurs;
 use App\Form\InscriptionType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SecurityController extends AbstractController
 {
   /**
   * @Route("/inscription", name="security_inscription")
   */
-    public function inscription() {
+    public function inscription(Request $request, EntityManagerInterface $manager) {
       
         $utilisateurs = new Utilisateurs();
       
         $form = $this->createForm(InscriptionType::class, $utilisateurs);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($utilisateurs);
+            $manager->flush();
+        }
       
         return $this->render('security/inscription.html.twig', [
           'form' => $form->createView()
--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -6,6 +6,7 @@ use App\Entity\Utilisateurs;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class InscriptionType extends AbstractType
 {
@@ -14,9 +15,9 @@ class InscriptionType extends AbstractType
         $builder
             ->add('email')
             ->add('pseudo')
-            ->add('motdepasse')
-            ->add('confirm_motdepasse')
-        ;
+            ->add('motdepasse', PasswordType::class)
+            ->add('confirm_motdepasse', PasswordType::class)
+        ; // PasswordType est une classe de Symfony qui permet de cacher les mots de passe par des puces.
     }
 
     public function configureOptions(OptionsResolver $resolver)
