60a26ee82795a586ec064d1a7837ee455cbff915
Gwennael Jean <gwennael.jean@gmail.com>
2020-06-22 19:04:29 +0200

    chore(inscription): utilisation de la propriété imageFile plutôt que avatar

--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Utilisateurs;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -17,7 +18,7 @@ class InscriptionType extends AbstractType
             ->add('pseudo')
             ->add('motdepasse', PasswordType::class)
             ->add('confirm_motdepasse', PasswordType::class)
-            ->add('avatar')
+            ->add('imageFile', FileType::class)
         ; // PasswordType est une classe de Symfony qui permet de cacher les mots de passe par des puces.
     }
 
