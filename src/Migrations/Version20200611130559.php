6ba4010971d053d8539c0663e7136b0418f1f1d4
Lolo8894 <lauramassaro.ks@gmail.com>
2020-08-12 17:37:35 +0200

    tri dans dossier images + remplacement des img créations dans easyadmin par des place-hold.it

---
+++ src/Migrations/Version20200611130559.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200611130559 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE creations ADD id INT AUTO_INCREMENT NOT NULL, DROP titre, DROP image, ADD PRIMARY KEY (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE creations MODIFY id INT NOT NULL');
+        $this->addSql('ALTER TABLE creations DROP PRIMARY KEY');
+        $this->addSql('ALTER TABLE creations ADD titre TEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_general_ci`, ADD image TEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_general_ci`, DROP id');
+    }
+}
